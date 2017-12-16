e45bbc76d71f745bcef286e7fdc8ed70c335edfc
Oliver Sõro <oliver.s6ro@gmail.com>
2017-12-16 17:14:52 +0200

    The backend/new-secret.sh will no also create postgrest.conf with a JWT secret.

--- backend/new-secret.sh
+++ backend/new-secret.sh
@@ -2,3 +2,6 @@
 secret="`openssl rand -base64 32`"
 echo "ALTER DATABASE elm_expense_manager SET \"app.jwt_secret\" TO '${secret}';" > ./schema/update-secret.sql
 
+cp ./postgrest-defaults.conf ./postgrest.conf
+echo "jwt-secret=\"$secret\"" >> postgrest.conf
+
