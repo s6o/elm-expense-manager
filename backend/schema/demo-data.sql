cb279cbb614062b2a44bf3c258aab7a79ce6b646
Oliver Sõro <oliver.s6ro@gmail.com>
2017-12-20 22:16:46 +0200

    More demo data: managers, accounts, categories etc.

--- backend/schema/demo-data.sql
+++ backend/schema/demo-data.sql
@@ -1,1 +1,62 @@
 insert into basic_auth.users (email, pass) values ('john.doe@lost.net', 'john2doe'), ('jane.doe@lost.net', 'jane2doe');
+
+
+insert into api.languages (name) values ('eng'), ('est');
+
+
+insert into api.managers (email, name, lang) values
+  ('jane.doe@lost.net', 'Jane Doe', 'eng')
+, ('john.doe@lost.net', 'John Doe', 'eng');
+
+
+insert into api.management_groups (name) values ('Lost Family');
+
+
+insert into api.management_group_members (group_id, member_id) values
+  (1, 1)
+, (1, 2);
+
+
+insert into api.currencies (currency_name, main_unit_name, sub_unit_name, iso_code, symbol)
+values
+  ('Island nuget', 'nuget', 'nuk', 'IUK', '~')
+;
+
+insert into api.accounts (mgr_id, currency_id, name, initial_balance, bank_account, bank_name)
+values
+  (1, 1, 'Jane''s account', '42000', 'ISL0912321092838481', 'Mount Banco Islando')
+, (2, 1, 'John''s account', '42000', 'ISL1204820390292221', 'Mount Banco Islando')
+;
+
+
+insert into api.payment_types (mgr_id, name) values
+  (1, 'Debit card')
+, (2, 'Debit card')
+;
+
+
+insert into api.categories (mgr_id, name, parent_path) values
+  (1, 'food', '/')
+, (1, 'meat', '/food')
+, (1, 'vegetables', '/food')
+, (1, 'children', '/')
+, (1, 'clothes', '/children')
+, (1, 'kindergarden', '/children')
+, (1, 'activities', '/children')
+, (1, 'salary', '/')
+, (2, 'food', '/')
+, (2, 'meat', '/food')
+, (2, 'vegetables', '/food')
+, (2, 'children', '/')
+, (2, 'clothes', '/children')
+, (2, 'kindergarden', '/children')
+, (2, 'activities', '/children')
+, (2, 'salary', '/')
+;
+
+
+insert into api.transactions (name) values
+  ('expense')
+, ('income')
+, ('transfer')
+;
